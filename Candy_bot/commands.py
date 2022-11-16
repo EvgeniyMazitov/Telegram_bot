e6fa4220eb0dfdcffa229afe1b314806a3060d9c
Evgeniy <>
2022-11-16 21:54:51 +0300

    add difficult

--- Candy_bot/commands.py
+++ Candy_bot/commands.py
@@ -15,17 +15,19 @@ async def start(message: types.Message):
     await view.hello(message)
     await asyncio.sleep(3)
     dp.register_message_handler(player_turn)
-    first_turn = random.randint(0,1)
+    first_turn = random.randint(0, 1)
     if first_turn:
         await await_player(player)
     else:
         await enemy_turn(player)
 
+
 async def player_turn(message: types.Message):
     player = message.from_user
+    max_take = model.get_max_take()
     model.set_player(player)
     if (message.text).isdigit():
-        if 0 < int(message.text) < 29:
+        if 0 < int(message.text) <= max_take:
             total_count = model.get_total_candies()
             player_take = int(message.text)
             total = total_count - player_take
@@ -43,12 +45,18 @@ async def player_turn(message: types.Message):
         await bot.send_message(message.from_user.id, f'{message.from_user.first_name}, '
                                                      f'вообще-то мы конфеты считаем в цифрах')
 
+
 async def enemy_turn(player):
     total_count = model.get_total_candies()
-    if total_count < 29:
+    max_take = model.get_max_take()
+    difficult = model.get_difficult()
+    if total_count <= max_take:
         enemy_take = total_count
     else:
-        enemy_take = (total_count - 1)%28
+        if difficult == 1:
+            enemy_take = (total_count - 1) % max_take
+        else:
+            enemy_take = random.randint(1, max_take)
     total = total_count - enemy_take
     await bot.send_message(player.id, f'Бот взял {enemy_take} конфет, '
                                       f'и на столе осталось {total}')
@@ -61,12 +69,11 @@ async def enemy_turn(player):
     await await_player(player)
 
 
-
-
 async def await_player(player):
     max_take = model.get_max_take()
     await bot.send_message(player.id,
                            f'{player.first_name}, бери конфеты, но не больше {max_take}')
+
 
 async def set_total_candies(message: types.Message):
 
@@ -75,12 +82,14 @@ async def set_total_candies(message: types.Message):
     await bot.send_message(message.from_user.id, f'Максимально количество конфет изменили на'
                                                  f' {count}')
 
+
 async def set_max_take(message: types.Message):
 
     count = int((message.text).split(" ")[1])
     model.set_max_take(count)
     await bot.send_message(message.from_user.id, f'Максимально количество конфет за ход изменили на'
                                                  f' {count}')
+
 
 async def set_difficult(message: types.Message):
 
